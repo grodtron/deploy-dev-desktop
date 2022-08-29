2443b551a9555dbd5cc53e90a776b2f2bc698a49
Gordon Bailey <gb@gordonbailey.net>
2022-08-29 05:06:19 +0000

    WIP getting CDK App working

--- infra/app.py
+++ infra/app.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 import os
 
+node_path = os.environ['BAZEL_NODE_BINARY']
+path = os.environ.get('PATH', '')
+
+path += ':' + os.path.dirname(node_path)
+
+os.environ['PATH'] = path
+
+print(os.getcwd())
+print(node_path)
+print(path)
+
 import aws_cdk as cdk
 
 from infra.infra_stack import InfraStack
@@ -20,7 +31,7 @@ InfraStack(app, "InfraStack",
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+    env=cdk.Environment(account='911866154296', region='eu-central-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
